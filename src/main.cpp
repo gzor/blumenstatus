e4690274c8672fe59ef3aa1ce703dfe33c4968c3
micha <micha.grueninger@alumni.fh-aachen.de>
2020-02-14 15:32:23 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "../lib/runningfastmedian.h"
+#include <DHT.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
